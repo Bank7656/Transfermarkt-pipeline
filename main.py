38f6117db8a123a61721fb7ed661d1322bb168ca
Bank7656 <thanapol6532@gmail.com>
2026-03-29 21:27:59 +0700

    Init

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+
+
+path = "https://www.kaggle.com/api/v1/datasets/download/davidcariboo/player-scores"
+
+
+
+def main():
+    print("Hello from de-project!")
+    print("Path to dataset files:", path)
+
+
+if __name__ == "__main__":
+    main()
